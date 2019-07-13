9edd01aee3629d85a4cda896698ff8bb31a893cd
chandan sharma <sharmachandan351@gmail.com>
2019-07-14 00:37:28 +0545

    django script injected

--- learnwithchandan/settings.py
+++ learnwithchandan/settings.py
@@ -123,3 +123,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+
+MEDIA_URL='/media/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+MEDIAFILES_DIRS = (
+    MEDIA_ROOT,
+)
+
--- learnwithchandan/urls.py
+++ learnwithchandan/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include,re_path
 from blog.views import ShowPosts,ShowDetail
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',include('blog.urls')),
     path('',ShowPosts.as_view(),name='show_posts'),
     re_path(r'^(?P<pk>\d+)/$',ShowDetail.as_view(),name='post_detail')
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
