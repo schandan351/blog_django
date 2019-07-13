63f6023c6096e8500e593523af9d34c182798787
chandan sharma <sharmachandan351@gmail.com>
2019-07-13 21:38:25 +0545

    models for posts added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+  title=models.CharField(max_length=100)
+  detail=models.TextField()
+  image=models.ImageField(upload_to='image/',blank=True)
+  author=models.CharField(max_length=100)
+  draft=models.BooleanField(default=True)
+  published_date=models.DateTimeField(auto_now_add=True)
+
+  def __str_(self):
+    return self.title
---
+++ static/js/mobile.js
@@ -0,0 +1,4 @@
+var navbar=document.querySelector(".navbar");
+document.querySelector(".mobile-icon").onclick=function(){
+    navbar.classList.toggle("active");
+}
